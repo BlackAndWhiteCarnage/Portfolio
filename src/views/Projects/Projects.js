f9e7b52da693e9c24c709e5ee74dbcf43ecd3c42
Krzysztof Repsch <krzys.repsch@gmail.com>
2021-08-28 15:41:34 +0200

    Added fully working Projects Section

--- src/views/Projects/Projects.js
+++ src/views/Projects/Projects.js
@@ -1,93 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
+// PROJECTS DATA
 import { projects } from 'data/projectsData';
-
-export const Wrapper = styled(motion.section)`
-  width: 100%;
-  min-height: 100vh;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`;
-
-export const ProjectsSliderWrapper = styled.div`
-  position: relative;
-  height: 450px;
-  width: 450px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  max-width: 1200px;
-  @media screen and (max-width: 1000px) {
-    width: 80%;
-  }
-`;
-
-export const Project = styled.div`
-  position: absolute;
-  opacity: 0;
-  width: 100%;
-  height: auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: ${({ theme }) => theme.colors.black};
-  color: ${({ theme }) => theme.colors.white};
-  left: 0;
-  transition: 0.5s ease;
-  transform: scale(0);
-  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034), 0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086), 0 100px 80px rgba(0, 0, 0, 0.12);
-  &.show,
-  &.next,
-  &.prev {
-    cursor: pointer;
-    transition: 0.5s ease;
-  }
-  &.show {
-    transform: scale(1);
-    opacity: 1;
-    left: 0;
-  }
-  &.next {
-    opacity: 0.2;
-    transform: scale(0.5);
-    left: 100%;
-  }
-  &.prev {
-    opacity: 0.2;
-    transform: scale(0.5);
-    left: -100%;
-  }
-`;
-
-const ProjectImage = styled.img`
-  width: 100%;
-  pointer-events: none;
-`;
-
-const ProjectTitle = styled.h2`
-  position: absolute;
-  top: 0;
-  text-align: right;
-  color: ${({ theme }) => theme.colors.black};
-  width: 100%;
-  pointer-events: none;
-  text-transform: uppercase;
-  opacity: 0;
-  transition: 0.5s ease;
-  &.show {
-    top: -80px;
-    transition: 0.8s ease;
-    opacity: 1;
-  }
-`;
+// HELPERS
+import { useScroll } from 'helpers/useScroll';
+// ANIMATIONS
+import { fade } from 'assets/animations/animation';
+// STYLES
+import { Wrapper, ProjectsSliderWrapper, Project, ProjectImage, ProjectTitle, SliderInfoWrapper, IsSliderLockedInfo } from './Projects.styles';
 
 const Projects = () => {
+  const [element, controls] = useScroll();
+
   const [clock, setClock] = useState(0);
+  const [isLocked, setIsLocked] = useState(false);
   let [current, setCurrent] = useState(0);
   let [next, setNext] = useState(projects.length - 1);
   let [prev, setPrev] = useState(current + 1);
@@ -100,25 +25,30 @@ const Projects = () => {
     setPrev(prev === projects.length - 1 ? 0 : prev + 1);
   };
 
-  //   const fireSlider = () => {
-  //     toggleInterval = setInterval(() => {
-  //       setClock(clock + 1);
-  //     }, 3000);
-  //     slider();
-  //   };
+  const fireSlider = () => {
+    if (isLocked) {
+      return;
+    }
+    toggleInterval = setInterval(() => {
+      setClock(clock + 1);
+    }, 2500);
+    slider();
+  };
 
-  //   useEffect(() => {
-  //     fireSlider();
-  //     return () => clearTimeout(toggleInterval);
-  //   }, [clock]);
+  useEffect(() => {
+    fireSlider();
+    return () => clearTimeout(toggleInterval);
+  }, [clock]);
 
   const projectHandler = (index) => {
     if (index === current) {
     } else if (next === index) {
+      setIsLocked(true);
       setCurrent(current === 0 ? (current = projects.length - 1) : current - 1);
       setNext(next === 0 ? (next = projects.length - 1) : next - 1);
       setPrev(prev === 0 ? (prev = projects.length - 1) : prev - 1);
     } else if (prev === index) {
+      setIsLocked(true);
       setCurrent(current === projects.length - 1 ? 0 : current + 1);
       setNext(next === projects.length - 1 ? 0 : next + 1);
       setPrev(prev === projects.length - 1 ? 0 : prev + 1);
@@ -126,7 +56,7 @@ const Projects = () => {
   };
 
   return (
-    <Wrapper>
+    <Wrapper variants={fade} animate={controls} initial='hidden' ref={element}>
       <ProjectsSliderWrapper>
         {projects.map((project, index) => (
           <Project
@@ -140,6 +70,16 @@ const Projects = () => {
             <ProjectTitle className={index === current && 'show'}>{project.title}</ProjectTitle>
           </Project>
         ))}
+        <SliderInfoWrapper>
+          <p>
+            <strong>Click</strong> on projects to interact.
+          </p>
+        </SliderInfoWrapper>
+        <IsSliderLockedInfo>
+          <p>
+            Slider <strong>{!isLocked ? 'unlocked' : 'locked'}</strong>
+          </p>
+        </IsSliderLockedInfo>
       </ProjectsSliderWrapper>
     </Wrapper>
   );
---
+++ src/views/Projects/Projects.styles.js
@@ -0,0 +1,140 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const Wrapper = styled(motion.section)`
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  overflow-x: hidden;
+`;
+
+export const ProjectsSliderWrapper = styled.div`
+  position: relative;
+  height: 450px;
+  width: 450px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 1200px;
+  @media screen and (max-width: 680px) {
+    width: 70%;
+    height: 250px;
+  }
+`;
+
+export const Project = styled.div`
+  position: absolute;
+  opacity: 0;
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${({ theme }) => theme.colors.white};
+  left: 0;
+  transition: 0.5s ease;
+  transform: scale(0);
+  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034), 0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
+    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086), 0 100px 80px rgba(0, 0, 0, 0.12);
+
+  &.show,
+  &.next,
+  &.prev {
+    cursor: pointer;
+    transition: 0.5s ease;
+  }
+  &.show {
+    transform: scale(1);
+    opacity: 1;
+    left: 0;
+    z-index: 20;
+    @media screen and (min-width: 1200px) {
+      &:hover {
+        transform: scale(1.05);
+      }
+    }
+  }
+  &.next,
+  &.prev {
+    opacity: 0.2;
+    transform: scale(0.5);
+    z-index: 10;
+    @media screen and (min-width: 1200px) {
+      &:hover {
+        opacity: 0.4;
+        transform: scale(0.55);
+      }
+    }
+  }
+  &.next {
+    left: 100%;
+    @media screen and (max-width: 1280px) {
+      left: 50%;
+    }
+  }
+  &.prev {
+    left: -100%;
+    @media screen and (max-width: 1280px) {
+      left: -50%;
+    }
+  }
+`;
+
+export const ProjectImage = styled.img`
+  width: 100%;
+  pointer-events: none;
+`;
+
+export const ProjectTitle = styled.h2`
+  position: absolute;
+  top: 0;
+  text-align: right;
+  color: ${({ theme }) => theme.colors.black};
+  width: 100%;
+  pointer-events: none;
+  text-transform: uppercase;
+  opacity: 0;
+  transition: 0.5s ease;
+  font-size: ${({ theme }) => theme.fontSize.m};
+  &.show {
+    top: -60px;
+    transition: 0.5s 0.5s ease;
+    opacity: 1;
+    @media screen and (max-width: 680px) {
+      top: -45px;
+    }
+  }
+  @media screen and (max-width: 680px) {
+    font-size: ${({ theme }) => theme.fontSize.s};
+  }
+`;
+
+export const SliderInfoWrapper = styled.p`
+  position: absolute;
+  bottom: -50px;
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media screen and (max-width: 680px) {
+    text-align: center;
+  }
+`;
+
+export const IsSliderLockedInfo = styled.p`
+  position: absolute;
+  bottom: -100px;
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media screen and (max-width: 680px) {
+    text-align: center;
+  }
+`;
